e6a39dac70f031fe5f20539f886ec081cfd52543
Sujeet-2184 <sujeet.ft252094@greatlakes.edu.in>
2026-03-17 21:54:44 +0530

    Add files via upload

---
+++ constants.tsx
@@ -0,0 +1,13 @@
+export const COLORS = {
+  primary: '#7f00cc', 
+  background: '#f5f5f5',
+  badgeText: '#0D7A40', 
+  badgeBorder: '#0D7A40', 
+  linkText: '#7f00cc', 
+  bannerBg: '#dfedfe'  
+};
+
+export const SLOT_CONFIG = {
+  'SLOT-1': '8AM-1PM',
+  'SLOT-2': '2PM-7PM'
+};
